625f9bb59314ee55ded0069aeed6e3376bb7bbbd
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2022-05-03 22:36:06 +0200

    refactor yaml renderer tests

--- tests/translators/yaml.py
+++ tests/translators/yaml.py
@@ -5,7 +5,13 @@ from glorpen.config import Schema
 from glorpen.config.translators.yaml import YamlRenderer
 
 
-def test_asd():
+def render(cls):
+    r = YamlRenderer()
+    model = Schema().generate(cls)
+    return r.render(model)
+
+
+def test_optional_fields():
     @dataclasses.dataclass
     class Dummy:
         comment_field: str = dataclasses.field(metadata={"doc": "some string value"})
@@ -18,10 +24,7 @@ def test_asd():
                                        }
         )
 
-    r = YamlRenderer()
-
-    model = Schema().generate(Dummy)
-    assert r.render(model) == """# some string value
+    assert render(Dummy) == """# some string value
 comment_field: # required str
 required_field: # required str
 nullable_field: ~
@@ -39,8 +42,4 @@ def test_multiline_defaults():
 line2
 line3"""
 
-    model = Schema().generate(Dummy)
-    r = YamlRenderer()
-    data = r.render(model)
-
-    assert data == "# field: |-\n#        line1\n#        line2\n#        line3\n"
+    assert render(Dummy) == "# field: |-\n#        line1\n#        line2\n#        line3\n"
